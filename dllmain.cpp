9c5ca529ed1d2193791b74aaed1c2fbec40b4b14
FLWL <12775045+FLWL@users.noreply.github.com>
2019-03-18 12:59:26 +0200

    Add command line arguments and a few additional API functions.

--- dllmain.cpp
+++ dllmain.cpp
@@ -13,17 +13,24 @@ rpc::server* srv;
 void EnableDebugConsole();
 void DisableDebugConsole();
 void LibraryThread();
+int GetRpcServerPort();
 void CreateRpcServer();
+void CheckCommandLineParameters();
 void HookGame();
 void UnhookGame();
 #include "funcs.h"
 
 int __fastcall GameThreadHook(GameStructs::Game* This, void* Unused);
 int __fastcall GameFocusHook(GameStructs::Game* This, void* Unused, void* wnd, unsigned int msg, unsigned int wparam, int lparam);
+bool __fastcall CheckMultiCopiesHook(GameStructs::Game* This, void* Unused);
+bool __fastcall StartRecordingHook(GameStructs::Game* This, void* Unused);
+void __fastcall InGameResolutionHook(GameStructs::Game* This, void* Unused);
 
 // Function that gets executed in a thread once our library has been loaded into the game
 void LibraryThread()
 {
+	//EnableDebugConsole();
+	CheckCommandLineParameters();
 	CreateRpcServer();
 	HookGame();
 }
@@ -31,23 +38,29 @@ void LibraryThread()
 // Set up a server that listens for RPCs
 void CreateRpcServer()
 {
-	srv = new rpc::server("127.0.0.1", 64720);
+	int RpcServerPort = GetRpcServerPort();
+	std::cout << "Initializing RPC server at port: " << RpcServerPort << std::endl;
+	srv = new rpc::server("127.0.0.1", RpcServerPort);
 
 	srv->bind("ResetGameSettings", &RpcFuncs::ResetGameSettings);
 	srv->bind("StartGame", &RpcFuncs::StartGame);
 	srv->bind("GetGameInProgress", &RpcFuncs::GetGameInProgress);
+	srv->bind("GetGameTime", &RpcFuncs::GetGameTime);
 	srv->bind("GetWinningPlayer", &RpcFuncs::GetWinningPlayer);
 	srv->bind("GetWinningPlayers", &RpcFuncs::GetWinningPlayers);
 	srv->bind("RestartGame", &RpcFuncs::RestartGame);
 	srv->bind("QuitGame", &RpcFuncs::QuitGame);
 	srv->bind("GetApiVersion", &RpcFuncs::GetApiVersion);
 	srv->bind("SetRunUnfocused", &RpcFuncs::SetRunUnfocused);
+	srv->bind("SetWindowMinimized", &RpcFuncs::SetWindowMinimized);
+	srv->bind("SetUseInGameResolution", &RpcFuncs::SetUseInGameResolution);
 	srv->bind("SetRunFullSpeed", &RpcFuncs::SetRunFullSpeed);
 
 	srv->bind("SetGameType", &RpcFuncs::SetGameType);
 	srv->bind("SetGameScenarioName", &RpcFuncs::SetGameScenarioName);
 	srv->bind("SetGameMapType", &RpcFuncs::SetGameMapType);
 	srv->bind("SetGameMapSize", &RpcFuncs::SetGameMapSize);
+	srv->bind("SetGameMapSizeDirectly", &RpcFuncs::SetGameMapSizeDirectly);
 	srv->bind("SetGameDifficulty", &RpcFuncs::SetGameDifficulty);
 	srv->bind("SetGameStartingResources", &RpcFuncs::SetGameStartingResources);
 	srv->bind("SetGamePopulationLimit", &RpcFuncs::SetGamePopulationLimit);
@@ -67,6 +80,15 @@ void CreateRpcServer()
 	srv->bind("SetPlayerTeam", &RpcFuncs::SetPlayerTeam);
 	srv->bind("GetPlayerExists", &RpcFuncs::GetPlayerExists);
 	srv->bind("GetPlayerAlive", &RpcFuncs::GetPlayerAlive);
+	srv->bind("GetPlayerScore", &RpcFuncs::GetPlayerScore);
+}
+
+void CheckCommandLineParameters()
+{
+	std::wstring CmdLine(GetCommandLine());
+
+	if (CmdLine.find(L"-multipleinstances") != -1)
+		RunMultipleInstances = true;
 }
 
 // Hook into the game thread
@@ -76,6 +98,9 @@ void HookGame()
 	DetourUpdateThread(GetCurrentThread());
 	DetourAttach(&(PVOID&)GameFuncs::TribeGame::_HandleIdle, GameThreadHook);
 	DetourAttach(&(PVOID&)GameFuncs::BaseGame::_HandleActivate, GameFocusHook);
+	DetourAttach(&(PVOID&)GameFuncs::BaseGame::_CheckMultiCopies, CheckMultiCopiesHook);
+	DetourAttach(&(PVOID&)GameFuncs::TribeGame::_StartCommandLogs, StartRecordingHook);
+	DetourAttach(&(PVOID&)GameFuncs::BaseGame::_SetResolutionToInGameResolution, InGameResolutionHook);
 	DetourTransactionCommit();
 }
 
@@ -86,6 +111,9 @@ void UnhookGame()
 	DetourUpdateThread(GetCurrentThread());
 	DetourDetach(&(PVOID&)GameFuncs::TribeGame::_HandleIdle, GameThreadHook);
 	DetourDetach(&(PVOID&)GameFuncs::BaseGame::_HandleActivate, GameFocusHook);
+	DetourDetach(&(PVOID&)GameFuncs::BaseGame::_CheckMultiCopies, CheckMultiCopiesHook);
+	DetourDetach(&(PVOID&)GameFuncs::TribeGame::_StartCommandLogs, StartRecordingHook);
+	DetourDetach(&(PVOID&)GameFuncs::BaseGame::_SetResolutionToInGameResolution, InGameResolutionHook);
 	DetourTransactionCommit();
 }
 
@@ -100,6 +128,31 @@ void DisableDebugConsole()
 {
 	fclose(DebugConsoleRedirect);
 	FreeConsole();
+}
+
+// Get RPC server port
+// Just something that gets the job done right now
+// This doesn't account for all cases, such as quotation marks around the parameter value
+int GetRpcServerPort()
+{
+	std::wstring CmdLine(GetCommandLine());
+	std::wstring Keyword = L"-autogameport ";
+
+	unsigned int Pos = CmdLine.find(Keyword);
+	if (Pos != -1)
+	{
+		Pos += Keyword.length();
+		std::wstring PortString = L"    ";
+		while (Pos < CmdLine.length() &&
+			((CmdLine[Pos] >= '0' && CmdLine[Pos] <= '9') || CmdLine[Pos] == ' '))
+		{
+			PortString += CmdLine[Pos++];
+		}
+
+		return _wtoi(PortString.c_str());
+	}
+
+	return 64720;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
@@ -144,4 +197,26 @@ int __fastcall GameFocusHook(GameStructs::Game* This, void* Unused, void* wnd, u
 		wparam = 1;
 
 	return GameFuncs::BaseGame::_HandleActivate(This, wnd, msg, wparam, lparam);
+}
+
+bool __fastcall CheckMultiCopiesHook(GameStructs::Game* This, void* Unused)
+{
+	if (!RunMultipleInstances)
+		return GameFuncs::BaseGame::_CheckMultiCopies(This);
+
+	return true; // make the multi copy check pass
+}
+
+bool __fastcall StartRecordingHook(GameStructs::Game* This, void* Unused)
+{
+	if (!RunMultipleInstances || RpcFuncs::GetGameRecorded()) // only start recording if the game should actually be recorded
+		return GameFuncs::TribeGame::_StartCommandLogs(This);
+
+	return true;
+}
+
+void __fastcall InGameResolutionHook(GameStructs::Game* This, void* Unused)
+{
+	if (UseInGameResolution)
+		GameFuncs::BaseGame::_SetResolutionToInGameResolution(This);
 }
--- funcs.h
+++ funcs.h
@@ -2,20 +2,46 @@
 #include <list>
 #include <sstream>
 
-const float API_VERSION = 1.14f;
+const float API_VERSION = 1.15f;
 
 bool RunUnfocused = false;
+bool UseInGameResolution = false;
+bool RunMultipleInstances = false;
 
 namespace GameStructs
 {
+	struct Game;
+	struct World;
+	struct Player;
+	struct VictoryConditions;
+	struct CommunicationsHandler;
+
 	struct Game
 	{
-		char unk1[44];
-		HWND* prog_window;
+		char unk1[48];
 		char unk2[432];
 		int prog_mode;
-		char unk3[3769];
+		char unk3[576];
+		World* world;
+		char unk4[3189];
 		char resigned[9];
+	};
+	struct World
+	{
+		char unk1[16];
+		unsigned int world_time;
+		char unk2[56];
+		Player** players;
+	};
+	struct Player
+	{
+		char unk1[132];
+		VictoryConditions* victory_conditions;
+	};
+	struct VictoryConditions
+	{
+		char unk1[28];
+		int total_victory_points;
 	};
 	struct CommunicationsHandler;
 
@@ -24,17 +50,16 @@ namespace GameStructs
 		ME_ABSENT = 0x0,
 		ME_CLOSED = 0x1,
 		ME_HUMAN = 0x2,
-		ME_ELIMINATED = 0x3,
-		ME_COMPUTER = 0x4,
-		ME_CYBORG = 0x5,
-		ME_VIEWONLY = 0x6,
+		ME_COMPUTER = 0x4
 	};
 
 	/* Game speedup offset by abductedPlatypus */
 	bool *DoFixedUpdate = (bool*)0x791208;
 
-	Game* GamePointer = *(Game**)0x7912A0;
-	CommunicationsHandler* CommPointer = *(CommunicationsHandler**)0x791200;
+	HWND *GameWindow = (HWND*)0x7911D8;
+	
+	Game* GetGamePointer() { return *(Game**)0x7912A0; };
+	CommunicationsHandler* GetCommPointer() { return *(CommunicationsHandler**)0x791200; };
 }
 
 namespace GameFuncs
@@ -53,6 +78,11 @@ namespace GameFuncs
 
 	namespace BaseGame
 	{
+		// General
+		bool(__thiscall* _CheckMultiCopies)(GameStructs::Game* This) = (bool(__thiscall*)(GameStructs::Game*))0x5E70A0;
+
+		void(__thiscall* _SetResolutionToInGameResolution)(GameStructs::Game* This) = (void(__thiscall*)(GameStructs::Game*))0x5EB5F0;
+
 		// Game funcs
 		void(__thiscall* _SetMultiplayerGame)(GameStructs::Game* This, bool bMultiplayer) = (void(__thiscall*)(GameStructs::Game*, bool))0x5EB1C0;
 		
@@ -65,11 +95,14 @@ namespace GameFuncs
 		void(__thiscall* _GameTeamsLocked)(GameStructs::Game* This, char Locked) = (void(__thiscall*)(GameStructs::Game*, char))0x5EB460;
 		
 		void(__thiscall* _SetRecordGame)(GameStructs::Game* This, int Record) = (void(__thiscall*)(GameStructs::Game*, int))0x5EB230;
+		bool(__thiscall* _GetRecordGame)(GameStructs::Game* This) = (bool(__thiscall*)(GameStructs::Game*))0x5EAEF0;
 		
 		void(__thiscall* _SetNumberPlayers)(GameStructs::Game* This, int NumberPlayers) = (void(__thiscall*)(GameStructs::Game*, int))0x5EB260;
 		
 		int(__thiscall* _HandleActivate)(GameStructs::Game* This, void* wnd, unsigned int msg, unsigned int wparam, int lparam) = (int(__thiscall*)(GameStructs::Game*, void*, unsigned int, unsigned int, int))0x5E8830;
 
+		void(__thiscall* _SetMapSize)(GameStructs::Game* This, char x, char y, char z) = (void(__thiscall*)(GameStructs::Game*, char, char, char))0x5EB1E0;
+		
 		// Player funcs
 		void(__thiscall* _SetPlayerTeam)(GameStructs::Game* This, int PlayerNum, char Team) = (void(__thiscall*)(GameStructs::Game*, int, char))0x5EB300;
 	}
@@ -77,8 +110,11 @@ namespace GameFuncs
 	namespace TribeGame
 	{
 		// Game thread hook
-		int(__thiscall* _HandleIdle)(void* This) = (int(__thiscall*)(void*))0x445BC0;
+		int(__thiscall* _HandleIdle)(GameStructs::Game* This) = (int(__thiscall*)(GameStructs::Game*))0x445BC0;
 		
+		// Record game hook
+		bool(__thiscall* _StartCommandLogs)(GameStructs::Game* This) = (bool(__thiscall*)(GameStructs::Game*))0x443960;
+
 		// General funcs
 		int(__thiscall* _StartGame)(GameStructs::Game* This, bool bSkipStartupScreens) = (int(__thiscall*)(GameStructs::Game*, bool))0x4420D0;
 		
@@ -122,30 +158,29 @@ namespace GameFuncs
 
 namespace RpcFuncs
 {
-	// Game funcs
 	void SetGameMultiPlayer(bool MultiPlayer)
 	{
-		GameFuncs::BaseGame::_SetMultiplayerGame(GameStructs::GamePointer, MultiPlayer);
+		GameFuncs::BaseGame::_SetMultiplayerGame(GameStructs::GetGamePointer(), MultiPlayer);
 	}
 
 	void SetGameTeamsTogether(bool TeamsTogether)
 	{
 		// Teams must be together in scenario maps right now, because the particular
 		// scenario team randomization function requires an UI element
-		if (GameFuncs::TribeGame::_ScenarioGame(GameStructs::GamePointer))
+		if (GameFuncs::TribeGame::_ScenarioGame(GameStructs::GetGamePointer()))
 		{
 			TeamsTogether = true;
 		}
 
-		GameFuncs::TribeGame::_SetRandomizePositions(GameStructs::GamePointer, !TeamsTogether);
+		GameFuncs::TribeGame::_SetRandomizePositions(GameStructs::GetGamePointer(), !TeamsTogether);
 	}
 	
 	void SetGameType(int GameType)
 	{
-		GameFuncs::TribeGame::_SetGameType(GameStructs::GamePointer, GameType);
+		GameFuncs::TribeGame::_SetGameType(GameStructs::GetGamePointer(), GameType);
 
 		// No support for randomizing positions in scenario maps right now
-		if (GameFuncs::TribeGame::_ScenarioGame(GameStructs::GamePointer))
+		if (GameFuncs::TribeGame::_ScenarioGame(GameStructs::GetGamePointer()))
 		{
 			SetGameTeamsTogether(true);
 		}
@@ -153,111 +188,120 @@ namespace RpcFuncs
 
 	void SetGameScenarioName(const std::string& ScenarioName)
 	{
-		GameFuncs::BaseGame::_SetScenarioName(GameStructs::GamePointer, ScenarioName.c_str());
+		GameFuncs::BaseGame::_SetScenarioName(GameStructs::GetGamePointer(), ScenarioName.c_str());
 	}
 
 	void SetGameMapType(int MapType)
 	{
-		GameFuncs::TribeGame::_SetMapType(GameStructs::GamePointer, MapType);
+		GameFuncs::TribeGame::_SetMapType(GameStructs::GetGamePointer(), MapType);
 	}
 
 	void SetGameMapSize(int MapSize)
 	{
-		GameFuncs::TribeGame::_SetMapSize(GameStructs::GamePointer, MapSize);
+		GameFuncs::TribeGame::_SetMapSize(GameStructs::GetGamePointer(), MapSize);
+	}
+
+	void SetGameMapSizeDirectly(int x, int y, int z)
+	{
+		GameFuncs::BaseGame::_SetMapSize(GameStructs::GetGamePointer(), x, y, z);
 	}
 
 	void SetGameDifficulty(int Difficulty)
 	{
-		GameFuncs::BaseGame::_SetDifficulty(GameStructs::GamePointer, Difficulty);
+		GameFuncs::BaseGame::_SetDifficulty(GameStructs::GetGamePointer(), Difficulty);
 	}
 
 	void SetGameStartingResources(int StartingResources)
 	{
-		GameFuncs::TribeGame::_SetResourceLevel(GameStructs::GamePointer, StartingResources);
+		GameFuncs::TribeGame::_SetResourceLevel(GameStructs::GetGamePointer(), StartingResources);
 	}
 
 	void SetGamePopulationLimit(int PopulationLimit)
 	{
-		GameFuncs::TribeGame::_SetPopLimit(GameStructs::GamePointer, PopulationLimit / 25);
+		GameFuncs::TribeGame::_SetPopLimit(GameStructs::GetGamePointer(), PopulationLimit / 25);
 	}
 
 	void SetGameRevealMap(int RevealMap)
 	{
-		GameFuncs::TribeGame::_SetVisibilityType(GameStructs::GamePointer, RevealMap);
+		GameFuncs::TribeGame::_SetVisibilityType(GameStructs::GetGamePointer(), RevealMap);
 	}
 
 	void SetGameStartingAge(int StartingAge)
 	{
-		GameFuncs::TribeGame::_SetStartingAge(GameStructs::GamePointer, StartingAge);
+		GameFuncs::TribeGame::_SetStartingAge(GameStructs::GetGamePointer(), StartingAge);
 	}
 
 	void SetGameVictoryType(int VictoryType, int VictoryValue)
 	{
-		GameFuncs::TribeGame::_SetVictoryType(GameStructs::GamePointer, VictoryType, VictoryValue);
+		GameFuncs::TribeGame::_SetVictoryType(GameStructs::GetGamePointer(), VictoryType, VictoryValue);
 	}
 
 	void SetGameTeamsLocked(bool Locked)
 	{
-		GameFuncs::BaseGame::_GameTeamsLocked(GameStructs::GamePointer, Locked);
+		GameFuncs::BaseGame::_GameTeamsLocked(GameStructs::GetGamePointer(), Locked);
 	}
 
 	void SetGameAllTechs(bool AllTechs)
 	{
-		GameFuncs::TribeGame::_SetFullTechTree(GameStructs::GamePointer, AllTechs);
+		GameFuncs::TribeGame::_SetFullTechTree(GameStructs::GetGamePointer(), AllTechs);
 	}
 
 	void SetGameRecorded(bool Record)
 	{
-		GameFuncs::BaseGame::_SetRecordGame(GameStructs::GamePointer, Record);
+		GameFuncs::BaseGame::_SetRecordGame(GameStructs::GetGamePointer(), Record);
 	}
 
-	// Player funcs
+	bool GetGameRecorded()
+	{
+		return GameFuncs::BaseGame::_GetRecordGame(GameStructs::GetGamePointer());
+	}
+	
 	void SetPlayerHuman(int PlayerNumber)
 	{
-		GameFuncs::CommHandler::_SetPlayerHumanity(GameStructs::CommPointer, PlayerNumber, GameStructs::PlayerHumanity::ME_HUMAN);
+		GameFuncs::CommHandler::_SetPlayerHumanity(GameStructs::GetCommPointer(), PlayerNumber, GameStructs::PlayerHumanity::ME_HUMAN);
 	}
 
 	void SetPlayerComputer(int PlayerNumber, const std::string& AIFile)
 	{
-		GameFuncs::CommHandler::_SetPlayerHumanity(GameStructs::CommPointer, PlayerNumber, GameStructs::PlayerHumanity::ME_COMPUTER);
-		GameFuncs::TribeGame::_SetComputerAIFile(GameStructs::GamePointer, PlayerNumber - 1, AIFile.c_str());
+		GameFuncs::CommHandler::_SetPlayerHumanity(GameStructs::GetCommPointer(), PlayerNumber, GameStructs::PlayerHumanity::ME_COMPUTER);
+		GameFuncs::TribeGame::_SetComputerAIFile(GameStructs::GetGamePointer(), PlayerNumber - 1, AIFile.c_str());
 	}
 
 	void SetPlayerComputerName(int PlayerNumber, const std::string& Name)
 	{
-		GameFuncs::TribeGame::_SetComputerName(GameStructs::GamePointer, PlayerNumber, Name.c_str());
+		GameFuncs::TribeGame::_SetComputerName(GameStructs::GetGamePointer(), PlayerNumber, Name.c_str());
 	}
 
 	void SetPlayerClosed(int PlayerNumber)
 	{
-		GameFuncs::CommHandler::_SetPlayerHumanity(GameStructs::CommPointer, PlayerNumber, GameStructs::PlayerHumanity::ME_CLOSED);
+		GameFuncs::CommHandler::_SetPlayerHumanity(GameStructs::GetCommPointer(), PlayerNumber, GameStructs::PlayerHumanity::ME_CLOSED);
 	}
 
 	void SetPlayerCivilization(int PlayerNumber, int Civilization)
 	{
-		GameFuncs::TribeGame::_SetCivilization(GameStructs::GamePointer, PlayerNumber - 1, Civilization);
+		GameFuncs::TribeGame::_SetCivilization(GameStructs::GetGamePointer(), PlayerNumber - 1, Civilization);
 	}
 
 	void SetPlayerColor(int PlayerNumber, int Color)
 	{
-		GameFuncs::TribeGame::_SetPlayerColor(GameStructs::GamePointer, PlayerNumber - 1, Color);
+		GameFuncs::TribeGame::_SetPlayerColor(GameStructs::GetGamePointer(), PlayerNumber - 1, Color);
 	}
 
 	void SetPlayerTeam(int PlayerNumber, int Team)
 	{
-		GameFuncs::BaseGame::_SetPlayerTeam(GameStructs::GamePointer, PlayerNumber - 1, Team + 1);
+		GameFuncs::BaseGame::_SetPlayerTeam(GameStructs::GetGamePointer(), PlayerNumber - 1, Team + 1);
 	}
 
 	bool GetPlayerExists(int PlayerNum)
 	{
-		GameStructs::PlayerHumanity Humanity = GameFuncs::CommHandler::_GetPlayerHumanity(GameStructs::CommPointer, PlayerNum);
+		GameStructs::PlayerHumanity Humanity = GameFuncs::CommHandler::_GetPlayerHumanity(GameStructs::GetCommPointer(), PlayerNum);
 		
 		return Humanity > GameStructs::PlayerHumanity::ME_CLOSED;
 	}
 
 	bool GetPlayerAlive(int PlayerNum)
 	{
-		return GetPlayerExists(PlayerNum) && !GameStructs::GamePointer->resigned[PlayerNum];
+		return GetPlayerExists(PlayerNum) && !GameStructs::GetGamePointer()->resigned[PlayerNum];
 	}
 
 	int GetWinningPlayer()
@@ -286,7 +330,7 @@ namespace RpcFuncs
 
 	bool GetGameInProgress()
 	{
-		int ProgMode = GameStructs::GamePointer->prog_mode;
+		int ProgMode = GameStructs::GetGamePointer()->prog_mode;
 
 		// 4 = active game
 		// 5 = game over
@@ -297,21 +341,21 @@ namespace RpcFuncs
 
 	bool GetGameOver()
 	{
-		int ProgMode = GameStructs::GamePointer->prog_mode;
+		int ProgMode = GameStructs::GetGamePointer()->prog_mode;
 
 		return ProgMode == 5;
 	}
 
 	bool GetGameStatsScreen()
 	{
-		int ProgMode = GameStructs::GamePointer->prog_mode;
+		int ProgMode = GameStructs::GetGamePointer()->prog_mode;
 
 		return ProgMode == 0;
 	}
 
 	void QuitGame()
 	{
-		GameFuncs::TribeGame::_QuitGame(GameStructs::GamePointer);
+		GameFuncs::TribeGame::_QuitGame(GameStructs::GetGamePointer());
 	}
 	
 	bool GetWorldLoaded()
@@ -327,12 +371,12 @@ namespace RpcFuncs
 		int NumPlayers = 0;
 		for (int i = 1; i <= 8; i++)
 		{
-			GameStructs::PlayerHumanity Humanity = GameFuncs::CommHandler::_GetPlayerHumanity(GameStructs::CommPointer, i);
+			GameStructs::PlayerHumanity Humanity = GameFuncs::CommHandler::_GetPlayerHumanity(GameStructs::GetCommPointer(), i);
 			
 			if (Humanity == GameStructs::PlayerHumanity::ME_HUMAN || Humanity == GameStructs::PlayerHumanity::ME_COMPUTER)
 				NumPlayers = i;
 		}
-		GameFuncs::BaseGame::_SetNumberPlayers(GameStructs::GamePointer, NumPlayers);
+		GameFuncs::BaseGame::_SetNumberPlayers(GameStructs::GetGamePointer(), NumPlayers);
 
 		void* DummyMPSetupScreen = operator new(0xDD0u);
 		bool LaunchResult = GameFuncs::TribeMPSetupScreen::_LaunchGame(DummyMPSetupScreen);
@@ -344,11 +388,10 @@ namespace RpcFuncs
 	{
 		if (GetWorldLoaded())
 		{
-			GameFuncs::TribeGame::_RestartGame(GameStructs::GamePointer);
+			GameFuncs::TribeGame::_RestartGame(GameStructs::GetGamePointer());
 		}
 	}
 
-	// General funcs
 	void ResetGameSettings()
 	{
 		// Quiting the game changes some of the settings, so in order to reset them
@@ -404,8 +447,34 @@ namespace RpcFuncs
 		RunUnfocused = Run;
 	}
 
+	void SetUseInGameResolution(bool InGameResolution)
+	{
+		UseInGameResolution = InGameResolution;
+	}
+
+	void SetWindowMinimized(bool Minimized)
+	{
+		ShowWindow(*GameStructs::GameWindow, Minimized ? SW_MINIMIZE : SW_SHOW);
+	}
+
 	void SetRunFullSpeed(bool FullSpeed)
 	{
 		*GameStructs::DoFixedUpdate = FullSpeed;
+	}
+
+	int GetGameTime()
+	{
+		if (!GetWorldLoaded())
+			return 0;
+
+		return GameStructs::GetGamePointer()->world->world_time / 1000;
+	}
+
+	int GetPlayerScore(int PlayerNumber)
+	{
+		if (!GetWorldLoaded() || !GetPlayerExists(PlayerNumber))
+			return 0;
+
+		return GameStructs::GetGamePointer()->world->players[PlayerNumber]->victory_conditions->total_victory_points;
 	}
 }
